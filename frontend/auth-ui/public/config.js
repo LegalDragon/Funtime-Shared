d6abd579c9236c5ab43ca95128e234e70c4aef97
Feng Xiao <feng@xiao.legal>
2025-12-29 10:02:25 -0500

    test stripe

--- frontend/auth-ui/public/config.js
+++ frontend/auth-ui/public/config.js
@@ -3,6 +3,6 @@
 window.__FUNTIME_CONFIG__ = {
   // API URL path - set to "/api" when backend is a virtual application
   // Leave empty only if API is at same origin root (not typical)
-  API_URL: "http://localhost:5000",
-  STRIPE_PUBLISHABLE_KEY: "pk_test_YOUR_KEY_HERE"
+  API_URL: "https://localhost:5001",
+  STRIPE_PUBLISHABLE_KEY: "pk_test_51Sjh30GYp7v2V1rfaXTN17VZ8S2XEy41j6xCLhm8RUtSjRn9rsCieuqGPfncNeoIyygAX8Gh7OmIpd0zJLx28UXY00unCayIRo"
 };
