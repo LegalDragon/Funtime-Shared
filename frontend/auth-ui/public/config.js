6e168c06317754ad62c10ad19e4719836398d38b
Feng Xiao <feng@xiao.legal>
2026-01-13 10:06:28 -0500

    tmptest

--- frontend/auth-ui/public/config.js
+++ frontend/auth-ui/public/config.js
@@ -6,7 +6,8 @@
 //
 window.__FUNTIME_CONFIG__ = {
   // API URL - use "/api" for same-origin deployment, full URL for cross-origin
-  API_URL: "/api",
+  API_URL: "https://localhost:5001",
+  //API_URL: "/api",
 
   // Stripe publishable key (test key for development, live key for production)
   STRIPE_PUBLISHABLE_KEY: "pk_test_51Sjh30GYp7v2V1rfaXTN17VZ8S2XEy41j6xCLhm8RUtSjRn9rsCieuqGPfncNeoIyygAX8Gh7OmIpd0zJLx28UXY00unCayIRo"
